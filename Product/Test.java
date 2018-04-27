638833c746e44b6c606b8fe846dd529610fe7202
leaflu0315 <johnson7511333@gmail.com>
2018-04-27 13:49:24 +0800

    Chinese -> Eng

--- Product/Test.java
+++ Product/Test.java
@@ -4,29 +4,30 @@ public class Test {
 
 	public static void main(String[] args) {
 		String name;
-		final int product_size = 100;
+		final int product_size = 100;   //MAXIMUM OBJECT = 100
 		int quantity,price,product_num = 0,id,total=0;
 		Scanner scan = new Scanner(System.in);
-		Product p[] = new Product[product_size];
+		Product p[] = new Product[product_size];    //100 Products
 		System.out.println("Please input the product name and price(q = quit)");
+		//insert the information
 		while(true){
 			name = scan.next();
 			if(name.equals("q")) break;
 			price = scan.nextInt();
 			p[product_num++] = new Product(name, price);
 		}
-		//êiâ›
+		//PURCHASE
 		while(true){
 			id = -1;
 			System.out.println("What product to purchase? (q to quit)");
 			name = scan.next();
 			if(name.equals("q")) break;
-			for(int i=0;i<product_num;i++) 
+			for(int i=0;i<product_num;i++)
 				if(p[i].getProductName().equals(name)){
-					id = i; 
+					id = i;
 					break;
 				}
-			if(id == -1){ 
+			if(id == -1){
 				System.out.println("Product Not Found");
 				continue;
 			}
@@ -34,18 +35,18 @@ public class Test {
 			quantity = scan.nextInt();
 			p[id].Purchase(quantity);
 		}
-		//ÊÃèo
+		//SELL
 		while(true){
 			System.out.println("What product to be sold?(q = quit)");
 			id = -1;
 			name = scan.next();
 			if(name.equals("q")) break;
-			for(int i=0;i<product_num;i++) 
+			for(int i=0;i<product_num;i++)
 				if(p[i].getProductName().equals(name)){
-					id = i; 
+					id = i;
 					break;
 				}
-			if(id == -1){ 
+			if(id == -1){
 				System.out.println("Product Not Found");
 				continue;
 			}
